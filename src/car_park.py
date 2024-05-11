d21425f8cac7d9f20dfa5f5268b1db509da8e489
20125282 <20125282@tafe.wa.edu.au>
2024-05-11 12:29:14 +0800

    Added constructors and attributes to the car park class

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,9 @@
 class CarPark:
-    pass
+    def __init__(self, location, capacity, plates=None, sensors=None, displays=None):
+        self.location = location
+        self.sensors = sensors or []
+        self.plates = plates or []
+        self.sensors = sensors or []
+        self.displays = displays or []
+    def __str__(self):
+        return "Car park at " + str(self.location) + ", with " + str(self.capacity) + " bays."
