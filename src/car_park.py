598887e58cde805f50b7246cbd78b814181e86e1
20125282 <20125282@tafe.wa.edu.au>
2024-05-30 22:02:48 +0800

    Added more methods to class CarPark

--- src/car_park.py
+++ src/car_park.py
@@ -1,25 +1,30 @@
 from sensor import Sensor
 from display import Display
-
+from pathlib import Path
+from datetime import datetime # we'll use this to timestamp entries
 
 class CarPark:
-    def __init__(self,
-                 location,
-                 capacity,
-                 plates=None,
-                 sensors=None,
-                 displays=None):
+    def __init__(self, location, capacity, plates=None, sensors=None, displays=None, log_file=Path("log.txt")):
         self.location = location
         self.capacity = capacity
         self.plates = plates or []
         self.sensors = sensors or []
         self.displays = displays or []
-
+        self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
+        # create the file if it doesn't exist:
+        self.log_file.touch(exist_ok=True)
     @property
     def available_bays(self):
         # car_park.available_bays
         return max(0, self.capacity - len(self.plates))
 
+    def add_car(self, plate):
+        self.plates.append(plate)
+        self.update_displays()
+        self._log_car_activity(plate, "entered")
+    def _log_car_activity(self, plate, action):
+        with self.log_file.open("a") as f:
+            f.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
     def __str__(self):
         return f'Welcome to {self.location} car park'
 
